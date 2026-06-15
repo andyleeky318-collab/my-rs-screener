ed302a352acf7d2861a9402257c356927e624b7a
andyleeky318-collab <andyleeky318@gmail.com>
2026-06-15 21:43:41 +0800

    QQQ

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ LIME_STOCKS = [
     'IPAY', 'ITB', 'JETS', 'KRE', 'KWEB', 'LIT', 'MAGS',
     'PBW', 'REMX', 'SHLD', 'SIL', 'SLX', 'SMH', 'TAN',
     'UFO', 'URA', 'USO', 'VTV', 'VUG', 'WGMI', 'XBI',
-    'XME', 'XRT', 'XTL', 'SPY'
+    'XME', 'XRT', 'XTL', 'SPY', 'QQQ'
 ]
 #'KBE', 'NLR', 'CLOU', 'XHB', 'BUG', 'HACK', 'ITA', 'IAT', 'XOP', 
 
