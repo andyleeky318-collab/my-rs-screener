40c9117819d309a5b19d58231a2a5d04491f29e9
andyleeky318-collab <andyleeky318@gmail.com>
2026-06-24 21:07:29 +0800

    RSP

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ LIME_STOCKS = [
     'IPAY', 'ITB', 'JETS', 'KRE', 'KWEB', 'LIT', 'MAGS',
     'PBW', 'REMX', 'SHLD', 'SIL', 'SLX', 'SMH', 'TAN',
     'UFO', 'URA', 'USO', 'VTV', 'VUG', 'WGMI', 'XBI',
-    'XME', 'XRT', 'XTL', 'SPY', 'QQQ'
+    'XME', 'XRT', 'XTL', 'SPY', 'QQQ', 'RSP'
 ]
 
 LIME_STOCKS1 = [
