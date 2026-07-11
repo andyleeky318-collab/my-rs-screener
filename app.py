172c15d50aa17f22c9b2e0b82c46c2a87db17442
andyleeky318-collab <andyleeky318@gmail.com>
2026-07-11 21:36:04 +0800

    CLR2

--- app.py
+++ app.py
@@ -6286,7 +6286,7 @@ if not setup_avgrank_hist.empty:
         legend=dict(orientation="h", yanchor="bottom", y=1.0, xanchor="center", x=0.5),
         xaxis=dict(showgrid=False, tickfont=dict(size=9, color="#888888")),
         yaxis=dict(title="Avg Rank", showgrid=True, gridcolor="rgba(120,120,120,0.15)", tickfont=dict(color="#888888")),
-        yaxis2=dict(title="Setup Count", overlaying="y", side="right", showgrid=False, tickfont=dict(color="#FF4B4B")),
+        yaxis2=dict(title="Setup Count", overlaying="y", side="right", showgrid=False, tickfont=dict(color="#888888")),
     )
     st.plotly_chart(fig_setup, use_container_width=True)
 else:
